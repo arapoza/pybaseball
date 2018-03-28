5bb84215ac9d36c4945ddc2ef1af372f06bcd3d7
Andrew Rapoza <andrapoza@gmail.com>
2018-03-28 16:50:34 -0400

    lookup only major league seasons for player

--- pybaseball/player_batting_stats.py
+++ pybaseball/player_batting_stats.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import io
 from bs4 import BeautifulSoup
 
 
@@ -10,14 +9,16 @@ def get_soup_fg(playerid):
     return BeautifulSoup(s, "html.parser")
 
 
-def get_table(soup, site):
+def get_table(soup):
     tables = soup.find_all('table')
+    # want the 10th table on the page, which is for standard stats
     table = tables[9]
     data = []
     headings = [th.get_text() for th in table.find("tr").find_all("th")][:]
     data.append(headings)
     table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
+    rows = table_body.find_all('tr', class_=['rgAltRow', 'rgRow'])
+    rows = [row for row in rows if not any(tag.startswith('grid') for tag in row['class'])]
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
@@ -28,15 +29,17 @@ def get_table(soup, site):
     return data
 
 
+def is_major_season(css_class):
+    return css_class == "rgAltRow" and not "grid" in css_class
+
 def batting_stats_player(playerid):
     """
-    Get all batting stats for a set time range. This can be the past week, the
-    month of August, anything. Just supply the start and end date in YYYY-MM-DD
-    format.
+    Get all batting stats for a player's career. Supply fangraphs playerid obtained from
+    playerid_lookup function.
     """
-    # retrieve html from baseball reference
+    # retrieve html from fangraphs
     soup = get_soup_fg(playerid)
-    table = get_table(soup, -1)
+    table = get_table(soup)
     table = table.dropna(how='all')  # drop if all columns are NA
     # scraped data is initially in string format.
     # convert the necessary columns to numeric.
@@ -47,3 +50,6 @@ def batting_stats_player(playerid):
         table[column] = pd.to_numeric(table[column])
         # table['column'] = table['column'].convert_objects(convert_numeric=True)
     return table
+
+
+print(batting_stats_player(14162))
